3783cefd753557e2f263af798e1a0968534b55aa
shiv <shivssharma1990@gmail.com>
2020-03-31 14:50:45 +0530

    created signup page

--- Pages/signup/Signup.js
+++ Pages/signup/Signup.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Icon, Content, Item, Input, View, Button } from 'native-base';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, KeyboardAvoidingView } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scrollview';
+import { SIGNUP_API_END_POINT } from '../../utils/constants';
 
 const Signup = () => {
+	const [ name, setName ] = useState('');
+	const [ email, setEmail ] = useState('');
+	const [ password, setPassword ] = useState('');
+
+	const signup = () => {
+		const body = {
+			name,
+			email,
+			password
+		};
+
+		fetch(SIGNUP_API_END_POINT, {
+			method: 'POST',
+			body: JSON.stringify(body),
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8'
+			}
+		})
+			.then((response) => {
+				console.log(response.status);
+				return response.json();
+			})
+			.then((json) => {
+				// props.navigation.navigate('SignupSuccess');
+				console.log(json);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
 	return (
+		// <KeyboardAwareScrollView>
 		<View style={styles.container}>
 			<View>
 				<Text style={styles.header}>CAR POOL</Text>
@@ -11,27 +45,32 @@ const Signup = () => {
 			<View style={styles.marginTop10}>
 				<Item rounded style={{ width: '80%' }}>
 					<Icon active type="MaterialCommunityIcons" name="account" />
-					<Input placeholder="Full Name" />
+					<Input placeholder="Full Name" onChange={(event) => setName(event.nativeEvent.text)} />
 				</Item>
 			</View>
 			<View style={styles.marginTop10}>
 				<Item rounded style={{ width: '80%' }}>
 					<Icon active type="FontAwesome5" name="envelope" />
-					<Input placeholder="E-Mail" />
+					<Input placeholder="E-Mail" onChange={(event) => setEmail(event.nativeEvent.text)} />
 				</Item>
 			</View>
 			<View style={styles.marginTop10}>
 				<Item rounded style={{ width: '80%' }}>
 					<Icon type="FontAwesome5" name="lock" />
-					<Input placeholder="Password" secureTextEntry={true} />
+					<Input
+						placeholder="Password"
+						secureTextEntry={true}
+						onChange={(event) => setPassword(event.nativeEvent.text)}
+					/>
 				</Item>
 			</View>
 			<View style={styles.marginTop20}>
-				<Button rounded style={styles.pHorizontal10}>
+				<Button rounded style={styles.pHorizontal10} onPress={signup}>
 					<Text style={styles.textWhite}>SignUp</Text>
 				</Button>
 			</View>
 		</View>
+		// </KeyboardAwareScrollView>
 	);
 };
 
